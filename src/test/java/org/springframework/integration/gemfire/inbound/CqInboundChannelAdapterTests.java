ec8856b603495ad7e5f26efaa8a6f60474d00489
Udo Kohlmeyer <udo@vmare.com>
2023-09-29 10:43:06 +1000

    SDGF-139: Converting Tests to use TestContainers

--- src/test/java/org/springframework/integration/gemfire/inbound/CqInboundChannelAdapterTests.java
+++ src/test/java/org/springframework/integration/gemfire/inbound/CqInboundChannelAdapterTests.java
@@ -5,90 +5,93 @@
 
 package org.springframework.integration.gemfire.inbound;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
+import com.vmware.gemfire.testcontainers.GemFireClusterContainer;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.query.CqEvent;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.integration.gemfire.fork.ForkUtil;
 import org.springframework.integration.test.util.TestUtils;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.io.OutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author David Turanski
  * @author Gary Russell
  * @author Artem Bilan
- *
  */
 @SpringJUnitConfig
 @DirtiesContext
 public class CqInboundChannelAdapterTests {
 
-	@Autowired
-	@Qualifier("test")
-	Region<String, Integer> region;
+    @Autowired
+    @Qualifier("test")
+    Region<String, Integer> region;
 
-	@Autowired
-	ConfigurableApplicationContext applicationContext;
+    @Autowired
+    ConfigurableApplicationContext applicationContext;
 
-	@Autowired
-	PollableChannel outputChannel1;
+    @Autowired
+    PollableChannel outputChannel1;
 
-	@Autowired
-	PollableChannel outputChannel2;
+    @Autowired
+    PollableChannel outputChannel2;
 
-	@Autowired
-	ContinuousQueryMessageProducer withDurable;
+    @Autowired
+    ContinuousQueryMessageProducer withDurable;
 
-	static OutputStream os;
+    private static GemFireClusterContainer gemFireClusterContainer;
 
-	@BeforeAll
-	public static void startUp() {
-		os = ForkUtil.cacheServer();
-	}
+    static OutputStream os;
 
-	@Test
-	public void testCqEvent() {
-		assertThat(TestUtils.getPropertyValue(withDurable, "durable", Boolean.class)).isTrue();
-		region.put("one", 1);
-		Message<?> msg = outputChannel1.receive(10000);
-		assertThat(msg).isNotNull();
-		assertThat(msg.getPayload() instanceof CqEvent).isTrue();
-	}
+    @BeforeAll
+    public static void startUp() {
+//		os = ForkUtil.cacheServer();
+        gemFireClusterContainer = new GemFireClusterContainer(1, "gemfire/gemfire:10.0.1");
 
-	@Test
-	public void testPayloadExpression() {
-		region.put("one", 1);
-		Message<?> msg = outputChannel2.receive(10000);
-		assertThat(msg).isNotNull();
-		assertThat(msg.getPayload()).isEqualTo(1);
-	}
+        gemFireClusterContainer.acceptLicense().start();
 
-	@AfterAll
-	public static void cleanUp() {
-		sendSignal();
-	}
+        gemFireClusterContainer.gfsh(
+                false,
+                "create region --name=test --type=REPLICATE");
 
-	public static void sendSignal() {
-		try {
-			os.write("\n".getBytes());
-			os.flush();
-		}
-		catch (IOException ex) {
-			throw new IllegalStateException("Cannot communicate with forked VM", ex);
-		}
-	}
+        System.setProperty("gemfire.locator.port",String.valueOf(gemFireClusterContainer.getLocatorPort()));
+
+    }
+
+    @Test
+    public void testCqEvent() {
+        assertThat(TestUtils.getPropertyValue(withDurable, "durable", Boolean.class)).isTrue();
+        region.put("one", 1);
+        Message<?> msg = outputChannel1.receive(10000);
+        assertThat(msg).isNotNull();
+        assertThat(msg.getPayload() instanceof CqEvent).isTrue();
+    }
+
+    @Test
+    public void testPayloadExpression() {
+        region.put("one", 1);
+        Message<?> msg = outputChannel2.receive(10000);
+        assertThat(msg).isNotNull();
+        assertThat(msg.getPayload()).isEqualTo(1);
+    }
+
+    @AfterAll
+    public static void cleanUp() {
+        sendSignal();
+    }
+
+    public static void sendSignal() {
+        gemFireClusterContainer.stop();
+    }
 
 }
