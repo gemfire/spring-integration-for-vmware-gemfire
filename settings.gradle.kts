d4ff16ee12311e876d140bdf2375900cc82a94e0
Udo Kohlmeyer <udo@vmare.com>
2023-12-29 10:59:10 +1100

    SDGF-193: Add version updating

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) VMware, Inc. 2023. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import java.io.FileInputStream
+import java.util.*
+
+pluginManagement {
+    includeBuild("build-tools/publishing")
+}
+rootProject.name = "spring-integration-gemfire"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val properties = Properties()
+            properties.load(FileInputStream("gradle.properties"))
+            versionOverrideFromProperties(this, properties)
+        }
+    }
+}
+
+private fun versionOverrideFromProperty(versionCatalogBuilder: VersionCatalogBuilder, propertyName: String, propertiesFile: Properties): String {
+    val propertyValue = System.getProperty(propertyName, propertiesFile.getProperty(propertyName))
+
+    return versionCatalogBuilder.version(propertyName, propertyValue)
+}
+
+private fun versionOverrideFromProperties(versionCatalogBuilder: VersionCatalogBuilder, properties: Properties) {
+    versionOverrideFromProperty(versionCatalogBuilder, "gemfireVersion", properties)
+    versionOverrideFromProperty(versionCatalogBuilder, "springDataGemfireVersion", properties)
+    versionOverrideFromProperty(versionCatalogBuilder, "springIntegrationVersion", properties)
+}
