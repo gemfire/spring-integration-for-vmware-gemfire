8dea59811163c558df9960deeca07015322988e5
Udo Kohlmeyer <udo@vmare.com>
2023-12-29 13:40:54 +1100

    SDGF-193: Fix incorrect version formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -125,7 +125,7 @@ private fun getBaseVersion(version: String): String {
     if (split.size < 2) {
         throw RuntimeException("version is malformed")
     }
-    return "$split[0].$split[1]"
+    return "${split[0]}.${split[1]}"
 }
 
 tasks.register("copyJavadocsToBucket") {
